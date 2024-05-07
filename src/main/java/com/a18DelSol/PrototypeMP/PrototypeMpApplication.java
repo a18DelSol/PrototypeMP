c6154daeec570759768f26b276cae2cc4251d7d4
a18DelSol <juanignacioiglesias00@gmail.com>
2024-05-06 23:34:53 -0300

    initial

---
+++ src/main/java/com/a18DelSol/PrototypeMP/PrototypeMpApplication.java
@@ -0,0 +1,13 @@
+package com.a18DelSol.PrototypeMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeMpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeMpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a18DelSol/PrototypeMP/PrototypeMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a18DelSol.PrototypeMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeMpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
